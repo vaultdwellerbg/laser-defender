485ae1f7591520e654a6aacba3e65ad509dc31bb
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-15 00:37:08 +0300

    Move enemy on set path

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,4 +4,35 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int currentWaypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[currentWaypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (currentWaypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[currentWaypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                currentWaypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
