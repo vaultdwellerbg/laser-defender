4b2b4e5e4f2e134b71ab0f5f713fe70fe35cc129
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-20 00:51:30 +0300

    Move waypoints from enemy to wave config, create new wave

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
     [SerializeField] float moveSpeed = 2f;
+    List<Transform> waypoints;
     int currentWaypointIndex = 0;
 
     private void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[currentWaypointIndex].transform.position;
     }
 
--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
@@ -11,7 +12,16 @@ public class WaveConfig : ScriptableObject
 	[SerializeField] float moveSpeed = 2f;
 
 	public GameObject GetEnemyPrefab() { return enemyPrefab; }
-	public GameObject GetPathPrefab() { return pathPrefab; }
+	public List<Transform> GetWaypoints() 
+	{
+		var waveWaypoints = new List<Transform>();
+		foreach (Transform child in pathPrefab.transform)
+		{
+			waveWaypoints.Add(child);
+		}
+
+		return waveWaypoints;
+	}
 	public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
 	public float GetSpawnRandomFactor() { return spawnRandomFactor; }
 	public int GetNumberOfEnemies() { return numberOfEnemies; }
