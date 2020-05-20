4e1e04a311f423a011bde8ee29800fb6b091b105
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-21 01:23:07 +0300

    Enable spawning of enemy wave

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWaveIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWaveIndex];
+        StartCoroutine(SpawnEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnEnemiesInWave(WaveConfig wave)
+    {
+        var numberOfEnemies = wave.GetNumberOfEnemies();
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            Instantiate(wave.GetEnemyPrefab(), wave.GetWaypoints()[0].transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(wave.GetTimeBetweenSpawns());
+        }
+    }
+}
