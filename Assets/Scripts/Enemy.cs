95b26a380f92a2dc311fb598374d41a06b04dc1b
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-24 19:55:41 +0300

    Add basic dealing of damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if (damageDealer != null)
+        {
+            health -= damageDealer.GetDamage();
+        }
+    }
+}
