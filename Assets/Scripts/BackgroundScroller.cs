da6eebbfb2784cca46b9fd1c6e8bf8016c6ef205
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-27 01:57:17 +0300

    Add moving background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material backgroundMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        backgroundMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    void Update()
+    {
+        backgroundMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
