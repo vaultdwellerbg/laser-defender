79a3f61167f453e29e4e8516bb319ac0e4e56c34
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-06-05 00:26:03 +0300

    Add base start/end scene and navigation between scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public void LoadGameScene() 
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void LoadGameOver()
+	{
+		SceneManager.LoadScene(SceneManager.sceneCount - 1);
+	}
+
+	public void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame() 
+	{
+		Application.Quit();
+	}
+}
