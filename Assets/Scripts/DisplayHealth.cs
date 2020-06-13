f2170e4217c1ab5384b3983e3e8ec9bc3b2e4916
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-06-13 22:24:41 +0300

    Display player health

---
+++ Assets/Scripts/DisplayHealth.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DisplayHealth : MonoBehaviour
+{
+    private Player player;
+    private TextMeshProUGUI healthText;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            healthText.text = player.GetHealth().ToString();
+        }
+        else 
+        {
+            healthText.text = "0";
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,6 +43,11 @@ public class Player : MonoBehaviour
         }
     }
 
+    public int GetHealth()
+    {
+        return health;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
