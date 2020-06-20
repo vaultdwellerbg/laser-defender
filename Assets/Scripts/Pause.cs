15000e803e3dcba277463d21b4d2e423ddf8675c
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-06-20 18:51:26 +0300

    Add game pausing functionality

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause")) 
+        {
+            TogglePaused();
+        }
+    }
+
+    private void TogglePaused()
+    {
+        Time.timeScale = Time.timeScale > 0f ? 0f : 1f;
+    }
+}
